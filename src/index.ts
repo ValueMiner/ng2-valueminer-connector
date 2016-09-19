e96dff35ebec4cabd325fc02d6edaab3645b0d0d
David Gaßmann <david.gassmann@gmail.com>
2016-09-19 20:07:29 +0200

    Small Bugfix: Did not export services

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { ValueMinerAPIModule } from './valueminerapi.module';
 export {OAuth2Config} from './interfaces';
 export * from './models/index';
-export * from './models/index';
+export * from './services/index';
