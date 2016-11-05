18bfb58967ef513bb512f317bb3ff79b99999ffe
David Gaßmann <david.gassmann@valueminer.eu>
2016-11-05 20:04:09 +0100

    Removed duplicate export

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -2,4 +2,3 @@ export { API } from './api.service';
 export { BackendService } from './backend.service';
 export { TokenService } from './token.service';
 export { BackendMessagingService } from './backend-messaging.service';
-export { BackendService } from './backend.service';
