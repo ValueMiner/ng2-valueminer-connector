2b7416e7140cfa059ed56e4683cab37eb4c60c8a
David Gaßmann <david.gassmann@valueminer.eu>
2016-11-14 13:08:08 +0100

    Exports nodestructures

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,4 +2,5 @@ export * from './instance.model';
 export * from './businessarea.model';
 export * from './model.model';
 export * from './relationship.model';
+export * from './node-structure.model';
 export * from './notification.model';
