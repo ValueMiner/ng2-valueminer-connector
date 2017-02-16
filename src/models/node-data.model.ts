127706b5423a7ffbae7ce099dcdd6c46d8eed9d6
Michael Weber <michael.weber@valueminer.eu>
2017-02-16 16:06:38 +0100

    Version: 0.7.1 - Added data to INodeDataAttributes

--- src/models/node-data.model.ts
+++ src/models/node-data.model.ts
@@ -2,6 +2,30 @@ import { JSONAPIResourceObject } from '../jsonapi/jsonapi-resource-object.model'
 
 export interface INodeDataAttributes {
     name?: string;
+    reference?: string;
+    crossReference?: string;
+    description?: string;
+    documentUri?: string;
+    status?: number;
+    responsibleId?: number;
+    targetDate?: string;
+    businessBenefit?: number;
+    commercialStatus?: number;
+    complexity?: number;
+    size?: number;
+    uncertainty?: number;
+    risk?: number;
+    budgetActual?: number;
+    budgetRemaining?: number;
+    benefitBudget?: number;
+    benefitActual?: number;
+    benefitRemaining?: number;
+    costBudget?: number;
+    costActual?: number;
+    costRemaining?: number;
+    investBudget?: number;
+    investActual?: number;
+    investRemaining?: number;
 }
 
 export interface INodeDataRelationships {
