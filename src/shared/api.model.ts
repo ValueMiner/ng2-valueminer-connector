3e7b3aa064987618cdc46fb3cee44cda83263b08
Tim Kerschbaumer <>
2017-12-28 11:54:50 +0100

    Updated duplicate functionality

--- src/shared/api.model.ts
+++ src/shared/api.model.ts
@@ -4,6 +4,7 @@ import { JSONAPIResourceObject } from '../jsonapi/jsonapi-resource-object.model'
 
 export interface IAPICreate<T extends JSONAPIResourceObject> {
   create(data: any): Observable<JSONAPIResponse<T>>;
+  duplicate(id: string, data: any): Observable<JSONAPIResponse<T>>;
   include(include: string[]): IAPICreate<T>;
 }
 
